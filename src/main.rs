5b4b5840630f933aac9e4b48bd2dabb4f50d35bd
Giles Knap <gilesknap@gmail.com>
2023-01-24 22:37:48 +0000

    gui render less often

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use eyes2::{gui, world};
 use std::{thread, time};
 
 fn main() {
-    let size = 25;
+    let size = 40;
     let grass_count = 10;
     let creature_count = 30;
 
@@ -13,9 +13,11 @@ fn main() {
 
     let mut gui = gui::EyesGui::new();
 
-    for _ in 0..30000 {
-        gui.render(&world);
+    for i in 0..30000 {
+        if i % 10 == 0 {
+            gui.render(&world);
+        }
         world.tick();
-        thread::sleep(time::Duration::from_millis(1));
+        thread::sleep(time::Duration::from_micros(100));
     }
 }
