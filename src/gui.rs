bb95f11e01136050d77c44cf9cab2522b79c1820
Giles Knap <giles.knap@diamond.ac.uk>
2023-01-24 12:58:01 +0000

    add ncurses support

--- src/gui.rs
+++ src/gui.rs
@@ -1,20 +1,29 @@
 use crate::entity::Cell;
 use crate::types::Position;
 use crate::world::World;
+extern crate pancurses;
+
+use pancurses::{endwin, initscr};
 
 // TODO rudimentary rendering of the world as a placeholder
 pub fn render(world: &World) {
-    println!();
-    println!("Rendering World of size: {}", world.get_size());
+    let window = initscr();
+
+    window.printw(format!("Rendering World of size: {}\n\n", world.get_size()));
 
     for y in 0..world.get_size() {
         for x in 0..world.get_size() {
             match world.get_cell(Position { x, y }) {
-                Cell::Empty => print!(" "),
-                Cell::Grass(_) => print!("*"),
-                Cell::Creature(_) => print!("o"),
+                Cell::Empty => window.printw(" "),
+                Cell::Grass(_) => window.printw("*"),
+                Cell::Creature(_) => window.printw("o"),
             };
         }
-        println!();
+        window.printw("\n");
     }
+    window.printw("\n");
+
+    window.refresh();
+    window.getch();
+    endwin();
 }
