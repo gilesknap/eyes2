6b46efd58e40c751aab55a37a08bdfe6b193030b
Giles Knap <gilesknap@gmail.com>
2023-01-26 18:14:00 +0000

    restore reset loop

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,25 @@ use eyes2::{gui, world};
 use std::{thread, time};
 
 fn main() {
-    let settings = Settings::load();
-    println!("{:?}", settings);
-    let mut world = world::World::new(settings);
+    loop {
+        let settings = Settings::load();
+        println!("{:?}", settings);
+        let mut world = world::World::new(settings);
 
-    world.populate();
+        world.populate();
 
-    let mut gui = gui::EyesGui::new();
+        let mut gui = gui::EyesGui::new();
 
-    for i in 0..1000000 {
-        if i % 1000 == 0 {
-            gui.render(&world);
+        for i in 0..100000000 {
+            if i % 1000 == 0 {
+                gui.render(&world);
+            }
+            world.tick();
+            thread::sleep(time::Duration::from_micros(1));
+            if world.creature_count() == 0 {
+                break;
+            }
         }
-        world.tick();
-        thread::sleep(time::Duration::from_micros(5));
-        // if world.creature_count() == 0 {
-        //     break;
-        // }
+        println!("Done! ({} ticks)", world.get_ticks());
     }
-    println!("Done! ({} ticks)", world.get_ticks());
 }
